93e3afd853ef0b1f534c45689c2232dddb709107
SGTMcClain <n8mcclain@gmail.com>
2015-11-03 03:09:41 -0500

    Initial Commit

---
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMovement : MonoBehaviour
+{
+    Transform player;
+    PlayerHealth playerHealth;
+    EnemyHealth enemyHealth;
+    NavMeshAgent nav;
+
+
+    void Awake ()
+    {
+        player = GameObject.FindGameObjectWithTag ("Player").transform;  //finds the player in the scene
+        playerHealth = player.GetComponent <PlayerHealth> ();
+        enemyHealth = GetComponent <EnemyHealth> ();
+        nav = GetComponent <NavMeshAgent> ();
+    }
+
+
+    void Update ()
+    {
+        if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+        {
+            nav.SetDestination (player.position);  //move enemy towards the player
+        }
+        else
+        {
+            nav.enabled = false;
+        }
+    }
+}
